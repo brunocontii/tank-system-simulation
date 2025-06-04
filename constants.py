bd8293282c58aed1fb3a10e77088ecb4de58e0ba
Bruno Conti <brunocse.5@gmail.com>
2025-06-04 17:16:44 -0300

    add constants file

---
+++ constants.py
@@ -0,0 +1,17 @@
+# constants for the exercise
+
+# base area of the tank (m^2)
+A = 2.0
+
+# gravitational acceleration
+G = 9.8
+
+# discharge coefficients
+K_1 = 0.5
+K_2 = 0.8
+
+# critical height to activate the secondary valve
+H_C = 20
+
+# initial condition
+H_0 = 10
